d53eb130779efe2a9e8a737d60dcc6bccc7b84de
plans-coding <152713201+plans-coding@users.noreply.github.com>
2025-12-18 22:43:22 +0100

    Add files via upload

---
+++ queries/explore.sql
@@ -0,0 +1,13 @@
+SELECT
+    *,
+    CAST(julianday(ReturnDate) - julianday(DepartureDate) AS INTEGER) AS NumberOfDays,
+    SUBSTR(InnerId, 1, 1) AS TripDomain,
+    CAST(strftime('%Y', DepartureDate) AS INTEGER) AS TripYear,
+    (CAST(strftime('%Y', DepartureDate) AS INTEGER) / 10) * 10 AS TripDecade
+FROM
+    bewa_Overview
+WHERE
+    InnerId IS NOT NULL
+ORDER BY
+    RANDOM()
+LIMIT 3;
