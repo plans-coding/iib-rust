a3e8ecff5052da7ab2d2b266998da7cf3494a151
plans-coding <152713201+plans-coding@users.noreply.github.com>
2025-12-28 21:39:02 +0100

    Add files via upload

--- pkg/lib.d.ts
+++ pkg/lib.d.ts
@@ -21,10 +21,10 @@ export interface InitOutput {
   readonly rust_sqlite_wasm_realloc: (a: number, b: number) => number;
   readonly sqlite3_os_end: () => number;
   readonly sqlite3_os_init: () => number;
-  readonly wasm_bindgen__convert__closures_____invoke__h287c14aff19c8166: (a: number, b: number, c: any) => void;
-  readonly wasm_bindgen__closure__destroy__hbeb953b7a7c54932: (a: number, b: number) => void;
   readonly wasm_bindgen__convert__closures_____invoke__h3afcb9cace96ef0d: (a: number, b: number) => void;
   readonly wasm_bindgen__closure__destroy__h3c30d884b410c426: (a: number, b: number) => void;
+  readonly wasm_bindgen__convert__closures_____invoke__h287c14aff19c8166: (a: number, b: number, c: any) => void;
+  readonly wasm_bindgen__closure__destroy__hbeb953b7a7c54932: (a: number, b: number) => void;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_exn_store: (a: number) => void;
--- pkg/lib.js
+++ pkg/lib.js
@@ -233,12 +233,12 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
-function wasm_bindgen__convert__closures_____invoke__h287c14aff19c8166(arg0, arg1, arg2) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h287c14aff19c8166(arg0, arg1, arg2);
-}
-
 function wasm_bindgen__convert__closures_____invoke__h3afcb9cace96ef0d(arg0, arg1) {
     wasm.wasm_bindgen__convert__closures_____invoke__h3afcb9cace96ef0d(arg0, arg1);
+}
+
+function wasm_bindgen__convert__closures_____invoke__h287c14aff19c8166(arg0, arg1, arg2) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h287c14aff19c8166(arg0, arg1, arg2);
 }
 
 /**
