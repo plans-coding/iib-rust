bf8e4aa63017d210210fb845bd610eb64c42210c
plans-coding <152713201+plans-coding@users.noreply.github.com>
2025-12-30 01:02:09 +0100

    Add files via upload

--- queries/simple/search_event.sql
+++ queries/simple/search_event.sql
@@ -1,9 +1,30 @@
 SELECT
-    'EventQuery' AS Query,
-    *
-FROM
-    IIBb_Events
+    SUBSTR(o.InnerId, 1, 1) AS DomainAbbreviation,
+    o.OuterId AS OuterId,
+    e.InnerId,
+    o.OverallDestination AS OverallDestination,
+    e.Date AS Date,
+    e.Events AS Events,
+    e.Accommodation AS Accommodation,
+    e.AccommodationCountry AS AccommodationCountry,
+    e.AccommodationCoordinatesAccuracy AS AccommodationCoordinatesAccuracy,
+    e.AccommodationCoordinates AS AccommodationCoordinates,
+    o.ParticipantGroup AS ParticipantGroup,
+    e.TravelParticipants AS TravelParticipants,
+    e.AdditionalNotes AS AdditionalNotes,
+    e.CountriesDuringDay AS CountriesDuringDay
+FROM bewb_Events e
+JOIN bewa_Overview o ON e.InnerId = o.InnerId
 WHERE
 (
-    COALESCE(OuterId, '') || ' ' || COALESCE(OverallDestination, '') || ' ' || COALESCE(Date, '') || ' ' || COALESCE(Events, '') || ' ' || COALESCE(Accommodation, '') || ' ' || COALESCE(AccommodationCountry, '') || ' ' || COALESCE(ParticipantGroup, '') || ' ' || COALESCE(TravelParticipants, '') || ' ' || COALESCE(AdditionalNotes, '') || ' ' || COALESCE(CountriesDuringDay, '')
+    COALESCE(o.OuterId, '') || ' ' ||
+    COALESCE(o.OverallDestination, '') || ' ' ||
+    COALESCE(e.Date, '') || ' ' ||
+    COALESCE(e.Events, '') || ' ' ||
+    COALESCE(e.Accommodation, '') || ' ' ||
+    COALESCE(e.AccommodationCountry, '') || ' ' ||
+    COALESCE(o.ParticipantGroup, '') || ' ' ||
+    COALESCE(e.TravelParticipants, '') || ' ' ||
+    COALESCE(e.AdditionalNotes, '') || ' ' ||
+    COALESCE(e.CountriesDuringDay, '')
 ) LIKE '%/*_STRING_*/%';
